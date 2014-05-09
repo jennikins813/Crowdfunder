08d398adb774c8425238f8d1a4037855a5981aab
jennikins813 <jennikins813@yahoo.com>
2014-05-08 23:13:29 -0400

    Add simple form gem and updated some project views

--- crowdfunder/app/models/project.rb
+++ crowdfunder/app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ActiveRecord::Base
+	has_many :rewards
+	has_many :pledges
+	belongs_to :user
+
+	accepts_nested_attributes_for :rewards, :reject_if => :all_blank, :allow_destroy => true
+
 	validates :blurb, :title, :presence => true
 	validates :start_date, :end_date, :presence => true
 	validates :funding_goal, :numericality => {:only_integer => true}
--- crowdfunder/app/models/reward.rb
+++ crowdfunder/app/models/reward.rb
@@ -1,2 +1,6 @@
 class Reward < ActiveRecord::Base
+	belongs_to :project
+	has_many :pledges
+	has_many :users, through: :pledges
+
 end
